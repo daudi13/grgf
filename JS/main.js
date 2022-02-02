4177918d88ec743bfbd58d011e38a6425668e42d
daudi13 <davouma13@gmail.com>
2022-02-02 13:43:35 +0300

    Add speaker properties in an object in the .js file

--- JS/main.js
+++ JS/main.js
@@ -22,3 +22,47 @@ links.forEach(link => link.addEventListener('click', () => {
 	document.querySelector('body').style.overflow = 'auto';
 	closeBtn.classList.remove('active');
 }))
+
+const speakers = [{
+	speakerName: 'Micheal Tai',
+	speakerPic: `./images/speakers/photo1.png`,
+	speakerAlt: `Micheal TaiHeadeshot`,
+	speakerOrigin: 'Berkham Proffessor of Entrepreurial legal development studies at the university of Nairobi',
+	speakerBio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ducimus corrupti natus consectetur, enim fugiat iusto sapiente dolores nesciunt? Cum quae fugiat iste, aliquam eligendi quo, incidunt dolore recusandae quis nihil enim magni ratione!',
+},
+	{
+	speakerName: 'Betty Michy',
+	speakerPic: `./images/speakers/photo2.png`,
+	speakerAlt: `Betty MichyHeadeshot`,
+	speakerOrigin: 'Berkham Proffessor of Entrepreurial legal development studies at the university of Nairobi',
+	speakerBio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ducimus corrupti natus consectetur, enim fugiat iusto sapiente dolores nesciunt? Cum quae fugiat iste, aliquam eligendi quo, incidunt dolore recusandae quis nihil enim magni ratione!',
+	},
+	{
+	speakerName: 'Yokazuka Mura',
+	speakerPic: `./images/speakers/photo3.png`,
+	speakerAlt: `Yokazuka Mura Headeshot`,
+	speakerOrigin: 'Berkham Proffessor of Entrepreurial legal development studies at the university of Nairobi',
+	speakerBio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ducimus corrupti natus consectetur, enim fugiat iusto sapiente dolores nesciunt? Cum quae fugiat iste, aliquam eligendi quo, incidunt dolore recusandae quis nihil enim magni ratione!',
+	},
+	{
+	speakerName: 'Doreen Benkler',
+	speakerPic: `./images/speakers/photo4.png`,
+	speakerAlt: `Doreen Benkler Headeshot`,
+	speakerOrigin: 'Berkham Proffessor of Entrepreurial legal development studies at the university of Nairobi',
+	speakerBio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ducimus corrupti natus consectetur, enim fugiat iusto sapiente dolores nesciunt? Cum quae fugiat iste, aliquam eligendi quo, incidunt dolore recusandae quis nihil enim magni ratione!',
+	},
+	{
+	speakerName: 'Brian Mulunya',
+	speakerPic: `./images/speakers/photo5.png`,
+	speakerAlt: `Brian Mulunya Headeshot`,
+	speakerOrigin: 'Berkham Proffessor of Entrepreurial legal development studies at the university of Nairobi',
+	speakerBio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ducimus corrupti natus consectetur, enim fugiat iusto sapiente dolores nesciunt? Cum quae fugiat iste, aliquam eligendi quo, incidunt dolore recusandae quis nihil enim magni ratione!',
+	},
+	{
+	speakerName: 'Johana Blaze',
+	speakerPic: `./images/speakers/photo6.png`,
+	speakerAlt: `Johana Blaze Headeshot`,
+	speakerOrigin: 'Berkham Proffessor of Entrepreurial legal development studies at the university of Nairobi',
+	speakerBio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ducimus corrupti natus consectetur, enim fugiat iusto sapiente dolores nesciunt? Cum quae fugiat iste, aliquam eligendi quo, incidunt dolore recusandae quis nihil enim magni ratione!',
+}
+]
