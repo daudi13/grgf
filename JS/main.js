3ab51d4a9e0146073772d57c85139416dee9263f
daudi13 <davouma13@gmail.com>
2022-02-02 13:07:53 +0300

    Update .js file with links eventlistener

--- JS/main.js
+++ JS/main.js
@@ -3,15 +3,22 @@ const openMenu = document.querySelector('.header-btn');
 const closeMenu = document.querySelector('.close-btn');
 const mobileMenu = document.querySelector('.mobile-nav');
 const closeBtn = document.querySelector('.close-btn');
+const links = document.querySelectorAll('.link')
 
 openMenu.addEventListener('click', () => {
 	mobileMenu.classList.add('active');
 	closeBtn.classList.add('active');
 	document.querySelector('body').style.overflow = 'hidden'
-})
+});
 
 closeBtn.addEventListener('click', () => {
 	mobileMenu.classList.remove('active');
 	document.querySelector('body').style.overflow = 'auto';
 	closeBtn.classList.remove('active');
-})
+});
+
+links.forEach(link => link.addEventListener('click', () => {
+	mobileMenu.classList.remove('active');
+	document.querySelector('body').style.overflow = 'auto';
+	closeBtn.classList.remove('active');
+}))
